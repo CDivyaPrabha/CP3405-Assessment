60a874270be081ca192f9d7491aeea7d44efe84a
CDivyaPrabha <31094031+CDivyaPrabha@users.noreply.github.com>
2018-12-29 00:43:58 +0800

    Backend code is done for signing up and signing in.

--- JCUConnect/app/forms.py
+++ JCUConnect/app/forms.py
@@ -11,5 +11,39 @@ class SignUpForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     designation = RadioField('You are a', choices=[('1', 'Student'), ('2', 'Employer')])
     checkbox = BooleanField('I agree to the given terms and conditions')
-    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Create Account')
+
+
+class StudentLoginForm(FlaskForm):
+    student_username = StringField('Username')
+    student_password = PasswordField('Password')
+    student_submit = SubmitField('Sign In')
+
+
+class EmployerLoginForm(FlaskForm):
+    employer_username = StringField('Username')
+    employer_password = PasswordField('Password')
+    employer_submit = SubmitField('Sign In')
+
+
+class User():
+
+    def __init__(self, username):
+        self.username = username
+        self.email = None
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.username
+
+    @staticmethod
+    def validate_login(password_hash, password):
+        return check_password_hash(password_hash, password)
--- JCUConnect/app/routes.py
+++ JCUConnect/app/routes.py
@@ -1,29 +1,86 @@
 from flask import render_template, flash, redirect, request, url_for
 from app import app
-from app.forms import SignUpForm
+from app.forms import SignUpForm, StudentLoginForm, EmployerLoginForm, User
 from pymongo import MongoClient
+from werkzeug.security import generate_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 
 client = MongoClient("mongodb://DivyaPrabha:practicala4@ds031972.mlab.com:31972/jcuconnect")
 collection = client["jcuconnect"].User
 
-#login_manager = LoginManager()
-#login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 
-#@app.route('/')
+@login_manager.user_loader
+def load_user(user):
+    u = collection.find_one({"Username": user})
+    if not u:
+        return None
+    return User(u['Username'])
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    if current_user.is_authenticated:
+        user = current_user
+    else:
+        user = "guest"
+    posts = collection.find()
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignUpForm()
-    if form.validate_on_submit():
+    if request.method == 'POST' and form.validate_on_submit():
+        # check if user_name or email is taken
+        user = collection.find_one({"Username": form.username.data})
+        if user and User.validate_login(user['Password'], form.password.data):
+            flash("User already exists!", category='error')
+            return render_template('signup.html', title='signup', form=form)
+        pass_hash = generate_password_hash(form.password.data, method='pbkdf2:sha256')
         post_data = {
             'Username': form.username.data,
-            'Password': form.password.data,
+            'Password': pass_hash,
             'Email': form.email.data,
             'Designation': form.designation.data,
-            'Remember user': form.remember_me.data,
             'Checkbox': form.checkbox.data
         }
-        result = collection.insert_one(post_data)
+        collection.insert_one(post_data)
+        # log the user in
+        user = collection.find_one({"Username": form.username.data})
+        user_obj = User(user['Username'])
+        login_user(user_obj)
+        flash("Logged in successfully!", category='success')
+        #go to student/employer home page=>return redirect(request.args.get("next") or url_for("dashboard"))
     return render_template('signup.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    student_form = StudentLoginForm()
+    employer_form = EmployerLoginForm()
+    if request.method == 'POST':
+        if student_form.validate_on_submit() and student_form.student_submit.data:
+            user = collection.find_one({"Username": student_form.student_username.data})
+            print(user)
+            if user and User.validate_login(user['Password'], student_form.student_password.data):
+                user_obj = User(user['Username'])
+                login_user(user_obj)
+                flash("Logged in successfully!", category='success')
+                return redirect(url_for('index'))# go to student home page
+            flash("Wrong username or password!", category='error')
+        elif employer_form.validate_on_submit() and employer_form.employer_submit.data:
+            user = collection.find_one({"Username": employer_form.employer_username.data})
+            if user and User.validate_login(user['Password'], employer_form.employer_password.data):
+                user_obj = User(user['Username'])
+                login_user(user_obj)
+                flash("Logged in successfully!", category='success')
+                return redirect(url_for('index'))# go to employer home page
+            flash("Wrong username or password!", category='error')
+    return render_template('login.html', title='login', student_form=student_form, employer_form=employer_form)
