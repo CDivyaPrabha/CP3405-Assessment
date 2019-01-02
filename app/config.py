652843cda8c8a2c854ee4e3957cbd7f6ab589a4f
CDivyaPrabha <31094031+CDivyaPrabha@users.noreply.github.com>
2019-01-02 16:06:02 +0800

    Backend code for Post jobs story.

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    DEBUG = False
+    DEBUG = True
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MLABURI = "mongodb://DivyaPrabha:practicala4@ds031972.mlab.com:31972/jcuconnect"
     MLABDB1 = "User"
--- app/forms.py
+++ app/forms.py
@@ -30,10 +30,13 @@ class EmployerLoginForm(FlaskForm):
 
 class JobForm(FlaskForm):
     company_name = StringField('Company\'s name', validators=[DataRequired()])
-    company_picture = FileField('Picture', validators=[FileAllowed(['jpg', 'png'], 'Images only!'), FileRequired(u'File was empty!')])
-    job_description = TextAreaField('Description', validators=[DataRequired()])
+    job_designation = StringField('Designation', validators=[DataRequired()])
+    company_picture = FileField('Picture', validators=[FileAllowed(['jpg', 'png'], 'Images only!'),
+                                                       FileRequired(u'File was empty!')])
+    job_description = TextAreaField('Description', validators=[DataRequired()], render_kw={"rows": 8, "cols": 50})
     job_type = SelectField('Type of Job', choices=['Full-time job', 'Part-time job', 'Contract-based job'])
-    job_requirements = TextAreaField('Requirements', validators=[DataRequired()])
+    job_requirements = TextAreaField('Requirements', validators=[DataRequired()], render_kw={"rows": 8, "cols": 50})
+    post_job = SubmitField('Post job')
 
 
 class User():
